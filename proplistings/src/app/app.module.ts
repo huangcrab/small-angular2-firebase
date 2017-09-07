a59151b168ebce6a307dc3f2c87d341b9721a36b
sean <huangcrab@gmail.com>
2017-09-06 23:57:47 -0400

    auth

--- proplistings/src/app/app.module.ts
+++ proplistings/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseService} from './services/firebase.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -9,6 +14,21 @@ import { NarbarComponent } from './components/narbar/narbar.component';
 import { ListingComponent } from './components/listing/listing.component';
 import { AddListingComponent } from './components/add-listing/add-listing.component';
 import { EditListingComponent } from './components/edit-listing/edit-listing.component';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA9C23kDj7J_KHNRN84OIZpGgtgKhaTYJY',
+    authDomain: 'listing-ef472.firebaseapp.com',
+    databaseURL: 'https://listing-ef472.firebaseio.com',
+    projectId: 'listing-ef472',
+    storageBucket: 'listing-ef472.appspot.com',
+    messagingSenderId: '67390453913'
+  }
+};
 
 const appRoutes: Routes = [
   {
@@ -17,11 +37,15 @@ const appRoutes: Routes = [
   },
   {
     path:"listings",
-    component:ListingComponent
+    component:ListingsComponent
   },
   {
     path:"add-listing",
     component:AddListingComponent
+  },
+  {
+    path:"listing/:id",
+    component:ListingComponent
   }
 ]
 
@@ -38,9 +62,13 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- proplistings/src/app/components/narbar/narbar.component.ts
+++ proplistings/src/app/components/narbar/narbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import { FlashMessagesService } from 'angular2-flash-messages'
 
 @Component({
   selector: 'app-narbar',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./narbar.component.css']
 })
 export class NarbarComponent implements OnInit {
+  user:Observable<firebase.User>;
 
-  constructor() { }
+  constructor(
+    public afAuth: AngularFireAuth,
+    public flashMessage: FlashMessagesService
+  ) {
+    this.user = afAuth.authState;
+   }
 
   ngOnInit() {
   }
 
+  login(){
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+    this.flashMessage.show('You are logged out',{cssClass: 'alert-success', timeout:3000});
+  }
 }
